3a69e876845785d021ac0faa6e75f8fe149a2c6c
Zhanibek <janibek3394@mail.ru>
2024-10-09 11:44:58 +0500

    minor fixes

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -30,6 +30,10 @@ class ViewController: UIViewController {
         tableView.constrainToEdges(of: view, safe: true)
         stylyze()
         setActions()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         showHalykWidgetPage(with: "https://baas-test.halykbank.kz/auth")
     }
 
@@ -44,10 +48,10 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
 
-        CommonInformation.shared.setPartnersInfo(login: "gbdq", password: "1234") { [weak self] processes in
-            guard let processes else { return }
-            self?.processes = processes
-        }
+//        CommonInformation.shared.setPartnersInfo(login: "gbdq", password: "1234") { [weak self] processes in
+//            guard let processes else { return }
+//            self?.processes = processes
+//        }
     }
 
     private func showHalykWidgetPage(with url: String) {
--- HalykWidget/Modules/HalykWidgetController.swift
+++ HalykWidget/Modules/HalykWidgetController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import WebKit
 import Combine
-import OZLivenessSDK
 internal import HalykCore
 
 public enum Flow { case onBoarding, authorization, homepage }
@@ -17,13 +16,11 @@ public class HalykWidgetController: UIViewController {
 
     private let webView = MessageWebView()
     private let viewModel = HalykWidgetViewModel()
-    private var ozLivenessVC: UIViewController?
     private var url: String
 
     public init(url: String = "https://baas-test.halykbank.kz") {
         self.url = url
         super.init(nibName: nil, bundle: nil)
-        ForenzicConfigurator.configure()
     }
 
     override public func viewDidLoad() {
@@ -48,43 +45,43 @@ public class HalykWidgetController: UIViewController {
     }
 
     private func showLivenessPage() {
-        do {
-            let bundle = Bundle(for: HalykWidgetController.self)
-
-            try OZSDK.set(licenseBundle: bundle)
-            let ozLivenessVC = try OZSDK.createVerificationVCWithDelegate(self, actions: [.selfie])
-            self.ozLivenessVC = ozLivenessVC
-            present(ozLivenessVC, animated: true)
-        } catch {
-            dump(error)
-            print("Unable to create OZSDK")
-        }
+//        do {
+//            let bundle = Bundle(for: HalykWidgetController.self)
+//
+//            try OZSDK.set(licenseBundle: bundle)
+//            let ozLivenessVC = try OZSDK.createVerificationVCWithDelegate(self, actions: [.selfie])
+//            self.ozLivenessVC = ozLivenessVC
+//            present(ozLivenessVC, animated: true)
+//        } catch {
+//            dump(error)
+//            print("Unable to create OZSDK")
+//        }
     }
 
-    private func onDeviceLiveness(results: [OZMedia]) {
-        viewModel.analyzeDeviceLiveness(results: results) { [weak self] images in
-            guard let viewController = self else { return }
-
-            viewController.viewModel.makeMultiPartRequest()
-            OZSDK.cleanTempDirectory()
-            if let ozLivenessVC = viewController.ozLivenessVC {
-                ozLivenessVC.dismiss(animated: true)
-            }
-            viewController.viewModel.sendLivenessResult(webView: viewController.webView)
-        }
-    }
+//    private func onDeviceLiveness(results: [OZMedia]) {
+//        viewModel.analyzeDeviceLiveness(results: results) { [weak self] images in
+//            guard let viewController = self else { return }
+//
+//            viewController.viewModel.makeMultiPartRequest()
+//            OZSDK.cleanTempDirectory()
+//            if let ozLivenessVC = viewController.ozLivenessVC {
+//                ozLivenessVC.dismiss(animated: true)
+//            }
+//            viewController.viewModel.sendLivenessResult(webView: viewController.webView)
+//        }
+//    }
 
     public required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 }
 
- extension HalykWidgetController: OZLivenessDelegate {
-
-    public func onOZLivenessResult(results: [OZLivenessSDK.OZMedia]) {
-        onDeviceLiveness(results: results)
-    }
-
-    public func onError(status: OZLivenessSDK.OZVerificationStatus?) {}
-}
+// extension HalykWidgetController: OZLivenessDelegate {
+//
+//    public func onOZLivenessResult(results: [OZLivenessSDK.OZMedia]) {
+//        onDeviceLiveness(results: results)
+//    }
+//
+//    public func onError(status: OZLivenessSDK.OZVerificationStatus?) {}
+//}
 
 extension HalykWidgetController: HalykWidgetViewModelViewDelegate {
 
